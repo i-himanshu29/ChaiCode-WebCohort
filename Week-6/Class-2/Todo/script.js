303cdc68b495e9ceb8fd2938f851ba1a5360c0c7
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-03-06 12:59:24 +0530

    TODO DOM PROJECT

--- Week-6/Class-2/Todo/script.js
+++ Week-6/Class-2/Todo/script.js
@@ -1,23 +1,24 @@
+
 const addBtn = document.getElementById('add-btn');
-const todoBtn = document.getElementById('todo-input');
+const todoInput = document.getElementById('todo-input');
+
 const todoItemsContainer = document.getElementById('todo-items-container');
 
-addBtn.addEventListener('click',() => {
-    const value = todoInput.value;
+addBtn.addEventListener('click', () => {
+  const value = todoInput.value;
 
-    const li = document.createElement('li');
-    li.innerText = value;
+  const li = document.createElement('li'); // <li></li>
+  li.innerText = value; // <li> {value} </li>
 
+  const delButton = document.createElement('button');
+  delButton.innerText = 'X';
 
-    const delButton = document.createElement('button');
-    delButton.innerText = 'X';
-   
+  delButton.addEventListener('click', function () {
+    li.remove();
+  });
 
-    delButton.addEventListener('click',function () {
-        li.remove();
-    });
-    
-    li.appendChild(delButton);
-    todoItemsContainer.appendChild(li);
-    todoInput.value = '';
+  li.appendChild(delButton);
+
+  todoItemsContainer.appendChild(li);
+  todoInput.value = '';
 });
